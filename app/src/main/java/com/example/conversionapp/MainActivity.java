6d700dd62a9551f8a206c77c42d3cd3e6d70ae43
nickbevers <nick.bevers9@gmail.com>
2021-03-05 17:09:44 +0100

    Used only 1 class TempFragment for all other Fragments to use

--- app/src/main/java/com/example/conversionapp/MainActivity.java
+++ app/src/main/java/com/example/conversionapp/MainActivity.java
@@ -5,62 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 
-public class MainActivity extends AppCompatActivity implements FragmentA.FragmentAListener, FragmentB.FragmentBListener, FragmentC.FragmentCListener {
+public class MainActivity extends AppCompatActivity{
 
-    double inputVal;
-    double valFahrenheit;
-    double valCelsius;
-    double valKelvin;
-
-    @Override
-    public void onInputASend(String input) {
-        inputVal = Double.parseDouble(String.valueOf(input));
-        valFahrenheit = inputVal*1.8+32;
-        valKelvin = inputVal+273.15;
-
-        fragmentB.updateFahrenheit(valFahrenheit);
-        fragmentC.updateKelvin(valKelvin);
-    }
-
-    @Override
-    public void onInputBSend(String input) {
-        inputVal = Double.parseDouble(String.valueOf(input));
-        valCelsius = (inputVal-32)/1.8;
-        valKelvin = (inputVal+459.67)*5/9;
-
-        fragmentA.updateCelsius(valCelsius);
-        fragmentC.updateKelvin(valKelvin);
-    }
-
-    @Override
-    public void onInputCSend(String input) {
-        inputVal = Double.parseDouble(String.valueOf(input));
-        valCelsius = inputVal-273.15;
-        valFahrenheit = (inputVal*9/5)-459.67;
-
-        fragmentA.updateCelsius(valCelsius);
-        fragmentB.updateFahrenheit(valFahrenheit);
-    }
-
-
-    private FragmentA fragmentA;
-    private FragmentB fragmentB;
-    private FragmentC fragmentC;
-
+    private TempFragment fragmentCelsius;
+    private TempFragment fragmentFahrenheit;
+    private TempFragment fragmentKelvin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragmentA = new FragmentA();
-        fragmentB = new FragmentB();
-        fragmentC = new FragmentC();
+
+        fragmentCelsius = new TempFragment();
+        fragmentFahrenheit = new TempFragment();
+        fragmentKelvin = new TempFragment();
+
+        //Update Fahrenheit and kelvin from Celsius
+        fragmentCelsius.setListener(new TempFragment.FragmentListener() {
+            @Override
+            public void onInputSend(String input) {
+                fragmentFahrenheit.updateTemp(input);
+                fragmentKelvin.updateTemp(input);
+            }
+        });
+
+        fragmentFahrenheit.setListener(new TempFragment.FragmentListener() {
+            @Override
+            public void onInputSend(String input) {
+
+            }
+        });
+
+        fragmentKelvin.setListener(new TempFragment.FragmentListener() {
+            @Override
+            public void onInputSend(String input) {
+
+            }
+        });
+
 
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.layoutA, fragmentA)
-                .replace(R.id.layoutB, fragmentB)
-                .replace(R.id.layoutC, fragmentC)
+                .replace(R.id.layoutA, fragmentCelsius)
+                .replace(R.id.layoutB, fragmentFahrenheit)
+                .replace(R.id.layoutC, fragmentKelvin)
                 .commit();
     }
 
---
+++ app/src/main/java/com/example/conversionapp/TempFragment.java
@@ -0,0 +1,81 @@
+package com.example.conversionapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class TempFragment extends Fragment {
+
+    private Object TextWatcher;
+    private EditText etTemp;
+    private FragmentListener listener;
+
+    public interface FragmentListener{
+        void onInputSend(String input);
+
+    }
+
+
+    public TempFragment() {
+        // Required empty public constructor
+    }
+
+    private final TextWatcher watcherCelsius = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String input = etTemp.getText().toString();
+            //Stuur naar fragment B
+            if (etTemp.hasFocus()) {
+                if (etTemp.length() >= 0 && !TextUtils.isEmpty(etTemp.getText().toString())) {
+                    try {
+                        listener.onInputSend(input);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    listener.onInputSend("0");
+                }
+            }
+        }
+    };
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.temp_fragment, container, false);
+
+        etTemp = v.findViewById(R.id.et_celsius);
+        etTemp.addTextChangedListener(watcherCelsius);
+
+        return v;
+    }
+
+
+    public void setListener(FragmentListener listener){
+        this.listener = listener;
+    }
+
+    public void updateTemp(String input){
+        etTemp.setText(input);
+    }
+}
